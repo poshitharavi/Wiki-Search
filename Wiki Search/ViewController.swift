ff40f768078901743498faa5b97cf3a3991409be
poshitha <poshitha@poshithas-iMac.local>
2020-02-08 06:30:27 -0800

    initial commit

---
+++ Wiki Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wiki Search
+//
+//  Created by poshitha on 2/8/20.
+//  Copyright © 2020 poshitha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wiki SearchTests/Wiki_SearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wiki_SearchTests.swift
+//  Wiki SearchTests
+//
+//  Created by poshitha on 2/8/20.
+//  Copyright © 2020 poshitha. All rights reserved.
+//
+
+import XCTest
+@testable import Wiki_Search
+
+class Wiki_SearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
